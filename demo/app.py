364be17676a9b5c654599628b4cd86cb6d9a9d4d
Patrick Rathje <git@patrickrathje.de>
2025-06-07 17:38:19 +0100

    some demo cleanup

--- demo/app.py
+++ demo/app.py
@@ -1,22 +1,12 @@
 
 import gradio as gr
 from pathlib import Path
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from gradio_motioncanvasplayer import MotionCanvasPlayer
 
 project_path = "/gradio_api/file=demo/public/project-3.17.2.js"
 
-# create a FastAPI app
-app = FastAPI()
-
-
-# mount FastAPI StaticFiles server
-#app.mount("/public", StaticFiles(directory='demo/public'), name="public")
-
-example = MotionCanvasPlayer().example_value()
 
 gr.set_static_paths(paths=[Path.cwd().absolute()/"demo/public"])
 
